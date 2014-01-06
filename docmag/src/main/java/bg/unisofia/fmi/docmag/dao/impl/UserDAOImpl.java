58fbd90c499c44932ce38325128a860918f87a3f
Georgi Krastev <joro.kr.21@gmail.com>
2014-01-06 22:44:03 +0200

    Refactored the PDF renderer

--- docmag/src/main/java/bg/unisofia/fmi/docmag/dao/impl/UserDAOImpl.java
+++ docmag/src/main/java/bg/unisofia/fmi/docmag/dao/impl/UserDAOImpl.java
@@ -19,47 +19,47 @@ import bg.unisofia.fmi.docmag.domain.impl.user.User.UserType;
 @Repository
 public class UserDAOImpl implements UserDAO {
 
-	@Autowired
-	private MongoTemplate mongoTemplate;
-	
-	private User getUser(String username) {
-		Query searchUserQuery = new Query(Criteria.where("username").is(username));
-		User user = mongoTemplate.findOne(searchUserQuery, User.class);
-		
-		return user;
-	}
-	
-	@Override
-	public void createUser(User user) {
-		 if (!mongoTemplate.collectionExists(User.class)) {
-	            mongoTemplate.createCollection(User.class);
-	        }       
-	        mongoTemplate.insert(user);
-	}
+        @Autowired
+        private MongoTemplate mongoTemplate;
+        
+        private User getUser(String username) {
+                Query searchUserQuery = new Query(Criteria.where("username").is(username));
+                User user = mongoTemplate.findOne(searchUserQuery, User.class);
+                
+                return user;
+        }
+        
+        @Override
+        public void createUser(User user) {
+                 if (!mongoTemplate.collectionExists(User.class)) {
+                    mongoTemplate.createCollection(User.class);
+                }       
+                mongoTemplate.insert(user);
+        }
 
-	@Override
-	public User getUserByUsername(String username) {
-		Query searchUserQuery = new Query(Criteria.where("username").is(username));
-		UserType userType = getUser(username).getType();
-		
-		switch (userType) {
-		case Student:
-			return mongoTemplate.findOne(searchUserQuery, Student.class);
-		case PHD:
-			return mongoTemplate.findOne(searchUserQuery, PHDStudent.class);
-		case Teacher:
-			return mongoTemplate.findOne(searchUserQuery, Teacher.class);
-		}
-		
-		return null;
-	}
+        @Override
+        public User getUserByUsername(String username) {
+                Query searchUserQuery = new Query(Criteria.where("username").is(username));
+                UserType userType = getUser(username).getType();
+                
+                switch (userType) {
+                case Student:
+                        return mongoTemplate.findOne(searchUserQuery, Student.class);
+                case PHD:
+                        return mongoTemplate.findOne(searchUserQuery, PHDStudent.class);
+                case Teacher:
+                        return mongoTemplate.findOne(searchUserQuery, Teacher.class);
+                }
+                
+                return null;
+        }
 
 
-	@Override
-	public List<User> getAllUsersOfType(UserType type) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        @Override
+        public List<User> getAllUsersOfType(UserType type) {
+                // TODO Auto-generated method stub
+                return null;
+        }
 
 
 }
--- docmag/src/main/java/bg/unisofia/fmi/docmag/pdf/Template.java
+++ docmag/src/main/java/bg/unisofia/fmi/docmag/pdf/Template.java
@@ -1,23 +1,23 @@
 package bg.unisofia.fmi.docmag.pdf;
 
-import bg.unisofia.fmi.docmag.domain.impl.document.Document;
+import bg.unisofia.fmi.docmag.domain.impl.document.Document.DocumentType;
 
 /**
  * Be careful to define only one template per class.
  */
 public enum Template {
-    THESIS_PROPOSITION(Document.class, "thesis_proposition.html");
+    THESIS_PROPOSAL(DocumentType.ThesisProposal, "thesis_proposal.html");
 
     private String file;
-    private Class<? extends Document> type;
+    private DocumentType type;
 
-    Template(Class<? extends Document> type, String file) {
+    Template(DocumentType type, String file) {
         String      path   = Constants.HTML_DIR + '/' + file;
         ClassLoader loader = getClass().getClassLoader();
         this.file          = loader.getResource(path).getFile();
         this.type          = type;
     }
 
-    public String               file() { return file; }
-    public Class<? extends Document> type() { return type; }
+    public String       file() { return file; }
+    public DocumentType type() { return type; }
 }
