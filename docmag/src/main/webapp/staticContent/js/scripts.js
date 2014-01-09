5ead68fe3aa78f53c13a5f7ba77921663a64ad0d
ttoncheva <tltoncheva@uni-sofia.bg>
2014-01-09 21:54:55 +0200

    added basic validation to the document forms

---
+++ docmag/src/main/webapp/staticContent/js/scripts.js
@@ -0,0 +1,32 @@
+function validate(inputClass,validationFieldId){
+	var isFormValid=false; 
+	var collectionLength=$('.'+inputClass).length;
+	
+	//input fields validation
+	$('.'+inputClass).focusout(function(){
+		var controlSum=0;
+		
+		if($(this).val() === ''){
+			$(this).parent().parent().addClass('has-error');
+		}
+		else{
+			$(this).parent().parent().removeClass('has-error');
+			
+			$('.'+inputClass).each(function(){
+
+				if($(this).val()!=''){
+					controlSum+=1;
+				}
+			});
+			
+			
+			if(collectionLength==controlSum){
+				isFormValid=true;
+			}
+		}
+		
+		$('#'+validationFieldId).val(isFormValid);
+	});
+	
+ 
+}
