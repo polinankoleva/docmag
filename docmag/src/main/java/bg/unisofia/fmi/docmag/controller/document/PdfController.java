5222bfde2f96b2a8de62e282c3292906ae9c1ac5
ttoncheva <tltoncheva@uni-sofia.bg>
2014-01-12 12:33:46 +0200

    solved conflict

--- docmag/src/main/java/bg/unisofia/fmi/docmag/controller/document/PdfController.java
+++ docmag/src/main/java/bg/unisofia/fmi/docmag/controller/document/PdfController.java
@@ -23,32 +23,19 @@ import bg.unisofia.fmi.docmag.service.UserService;
 @Controller
 public class PdfController {
 
-	private static final String DATE_PATTERN = "dd.MM.YYYY";
-	@Autowired UserService     usrService;
-	@Autowired DocumentService docService;
-	@RequestMapping(value  = "/thesisproposal", method = RequestMethod.GET)
-	public String printThesisProposal(@RequestHeader("User-Id") ObjectId userId, @RequestParam(value = "markdown", required = false,defaultValue = "false") boolean markdown, ModelMap model) {
-		User student  = usrService.getUserById(userId);
-		// TODO: add error handling if the user is not a student
-		ThesisProposal document = docService.getThesisProposalForUser(userId);
-		// TODO: add error handling if the document doesn't exist
-		List<Teacher> consultants  = usrService.getConsultantsForThesis(document);
-		List<Teacher> scientificLeaders =
-				usrService.getScientificLeadersForThesis(document);
-		// populate the model
-		if (markdown) model.put("markdown", new Markdown4jProcessor());
-		model.put("dateFormat", new SimpleDateFormat(DATE_PATTERN));
-		model.put("student", student);
-		model.put("document", document);
-		model.put("scientificLeaders", scientificLeaders);
-		model.put("consultants", consultants);
-		return "thesis_proposal";
-	}
+    private static final String DATE_PATTERN = "dd.MM.YYYY";
+    @Autowired UserService usrService;
+    @Autowired DocumentService docService;
 
-    @RequestMapping(value  = "/pdf/thesisproposal",
+    @RequestMapping(value = "/pdf/thesisproposal",
                     method = RequestMethod.GET)
-    public String printThesisProposal(@RequestHeader("User-Id") ObjectId userId,@RequestParam(value= "markdown",required = false,defaultValue = "false") boolean markdown,ModelMap model) {
-        Student student         = usrService.getUserById(userId);
+    public String printThesisProposal(
+            @RequestHeader("User-Id") ObjectId userId,
+            @RequestParam(value = "markdown",
+                          required = false,
+                          defaultValue = "false") boolean markdown,
+            ModelMap model) {
+        Student student = usrService.getUserById(userId);
         // TODO: add error handling if the user is not a student
         ThesisProposal document = docService.getThesisProposalForUser(userId);
         // TODO: add error handling if the document doesn't exist
