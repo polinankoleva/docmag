c817016ceb1d381399b49be8da338f6ccce410d6
polinankoleva <polina.n.koleva@gmail.com>
2014-01-12 21:41:01 +0200

    Add Response Status NO_CONTENT

--- docmag/src/main/java/bg/unisofia/fmi/docmag/controller/user/UserController.java
+++ docmag/src/main/java/bg/unisofia/fmi/docmag/controller/user/UserController.java
@@ -4,11 +4,13 @@ import java.util.Map;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import bg.unisofia.fmi.docmag.service.UserService;
 
@@ -30,11 +32,13 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/{userId}/thesisdefence", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void updateThesisDefenceForUser(@PathVariable ObjectId userId, @RequestParam ObjectId thesisDefenceId) {
 		userService.updateThesisdefenceForUser(thesisDefenceId, userId);
 	}
 	
 	@RequestMapping(value="/{userId}/thesisdefence", method = RequestMethod.DELETE)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void deleteThesisDefenceForUser(@PathVariable ObjectId userId) {
 		userService.deteleThesisdefenceForUser(userId);
 	}
@@ -45,14 +49,35 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/{userId}/thesisdefence/mark", method = RequestMethod.POST)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void setThesisDefenceMarkForUser(@PathVariable ObjectId userId, @RequestParam String mark) {
 		userService.setThesisDefenceMarkForStudentWithId(mark, userId);
 	}
 	
 	@RequestMapping(value="/{userId}/thesisdefence/mark", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void updateThesisDefenceMarkForUser(@PathVariable ObjectId userId, @RequestParam String mark) {
 		userService.updateThesisDefenceMarkForStudentWithId(mark, userId);
 	}
 	
+	@RequestMapping(value="/{userId}/recension", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void getRecensionForUser(@PathVariable ObjectId userId) {
+		//must be implement
+	}
+	
+	@RequestMapping(value="/{userId}/recension", method = RequestMethod.POST)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void setRecensionForUser(@PathVariable ObjectId userId) {
+		//must be implement
+		
+	}
+	
+	@RequestMapping(value="/{userId}/recension", method = RequestMethod.PUT)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void updateRecension(@PathVariable ObjectId userId, @RequestParam String mark) {
+		//must be implement
+
+	}
 	
 }
