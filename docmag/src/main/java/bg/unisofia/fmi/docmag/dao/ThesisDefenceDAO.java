c59f7acd8d57038571ee19034ccdcf753a892b1a
ttoncheva <tltoncheva@uni-sofia.bg>
2014-01-20 20:52:16 +0200

    GET for reports for teachers

--- docmag/src/main/java/bg/unisofia/fmi/docmag/dao/ThesisDefenceDAO.java
+++ docmag/src/main/java/bg/unisofia/fmi/docmag/dao/ThesisDefenceDAO.java
@@ -10,26 +10,26 @@ import bg.unisofia.fmi.docmag.domain.impl.user.Teacher;
 
 public interface ThesisDefenceDAO {
 
-	// POST, PUT
-	public ObjectId createThesisDefence(Date date);
+    // POST, PUT
+    public ObjectId createThesisDefence(Date date);
 
-	public ObjectId createThesisDefence(Date date,
-			List<ObjectId> commissionParticipantIds);
+    public ObjectId createThesisDefence(Date date,
+                    List<ObjectId> commissionParticipantIds);
 
-	public boolean assignTeacherToCommissionForThesisDefence(Teacher teacher,
-			ObjectId thesisDefenceId);
+    public boolean assignTeacherToCommissionForThesisDefence(Teacher teacher,
+                    ObjectId thesisDefenceId);
 
-	public void saveThesisDefence(ThesisDefence thesisDefence);
+    public void saveThesisDefence(ThesisDefence thesisDefence);
 
-	// GET
-	public List<ThesisDefence> getAllThesisDefences();
+    // GET
+    public List<ThesisDefence> getAllThesisDefences();
 
-	public ThesisDefence getThesisDefenceById(ObjectId id);
+    public ThesisDefence getThesisDefenceById(ObjectId id);
 
-	// DELETE
-	public void delete(ObjectId thesisDefenceId);
-	
-	//reports
-	public List<ThesisDefence> thesisDefencesBetweenDatesIncludingCommissionParticipants(
-			Date startDate, Date endDate, ObjectId commissionParticipantId);
+    // DELETE
+    public void delete(ObjectId thesisDefenceId);
+    
+    //reports
+    public List<ThesisDefence> thesisDefencesBetweenDatesIncludingCommissionParticipants(
+                    Date startDate, Date endDate, ObjectId commissionParticipantId);
 }
